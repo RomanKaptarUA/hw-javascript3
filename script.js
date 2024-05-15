2f9b2ce2392686f5355ee0287762042b4f140603
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-15 19:29:29 +0300

    ФІВАПР

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+// 1
 let a;
 a = 5 + 5 + '5';
 console.log(a);
 
-let my;
-let name;
-let is;
+// 2
+let email = 'romankaptar82@gmail.com';
+console.log(email.includes('@'));
+console.log(email.length);
 
-let fullName = `${my}, ${name}, ${is}`;
-console.log(fullName);
+// let my;
+// let name;
+// let is;
+
+// let fullName = `${my}, ${name}, ${is}`;
+// console.log(fullName);
+
+
+let FullName;
+FullName = 'my ' + 'name ' + 'is ' + 'Viktor';
+console.log(FullName);
+
+
+
+
+let UserName;
+let = 300;
+let payment = alert('Дякуємо, Олександро! До сплати 300 гривень');
+console.log('Дякуємо, Олександро! До сплати 300 гривень');
